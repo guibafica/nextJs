21fbaa33ad544f736a85456b21d661535655d7b1
Guilherme Bafica <gb.engcomp@hotmail.com>
2021-04-14 17:41:25 -0300

    CHORE: Client Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
-import { Title } from '../styles/pages/Home';
+import { useEffect, useState } from 'react';
+
+import { Container, Title } from '../styles/pages/Home';
+
+interface IProduct {
+  id: string;
+  title: string;
+  image: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      });
+    });
+  }, []);
+
   return (
-    <div>
-      <Title>Hello World!</Title>
-    </div>
+    <Container>
+      <section>
+        <Title>Recommended Products</Title>
+
+        <ul>
+          {recommendedProducts.map(product => {
+            return (
+              <li key={product.id}>
+                <img src={product.image} alt={product.title} />
+
+                {product.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </Container>
   )
 }
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  padding: 30px;
+  flex: 1;
+
+  li {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 50%;
+
+    & + li {
+      margin-top: 20px;
+    }
+  }
+
+  img {
+    width: 150px;
+    border-radius: 8px;
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
+
 export const Title = styled.h1`
   color: #8257e5;
+  margin-left: 50%;
 `;
