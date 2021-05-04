27c2ae257712f46eb4d5664005e5abf92d74458c
Guilherme Bafica <gb.engcomp@hotmail.com>
2021-05-04 16:17:12 -0300

    ADD: Dynamic Statics Pages

---
+++ src/pages/catalog/categories/[slug].tsx
@@ -0,0 +1,64 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+
+interface IProduct {
+  id: string;
+  title: string;
+  image: string;
+}
+
+interface CategoryProps {
+  products: IProduct[];
+}
+
+export default function Categories({ products }: CategoryProps) {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Page: {router.query.slug}</h1>
+
+      <ul>
+        {products.map(product => {
+          return (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+
+              {product.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('http://localhost:3333/categories');
+  const categories = await response.json();
+
+  const paths = categories.map(category => {
+    return {
+      params: { slug: category.id }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps<CategoryProps> = async (content) => {
+  const { slug } = content.params;
+
+  const response = await fetch(`http://localhost:3333/products?category_id=${slug}`);
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60,
+  }
+}
